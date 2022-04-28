ee0c95eb9d9cac99045d389417848cab7643dc45
zuzu <dlwnghks6821@naver.com>
2022-04-28 19:05:39 +0900

    # 롤전적검색 디코봇

--- discordbot/lib/src/main/java/discordbot/DiscordBot.java
+++ discordbot/lib/src/main/java/discordbot/DiscordBot.java
@@ -11,42 +11,46 @@ import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-
 import org.json.simple.JSONObject;
 
-public class DiscordBot extends ListenerAdapter{
+public class DiscordBot extends ListenerAdapter {
 	private boolean isCalled = false;
-	
-	private final static String token = "OTY5MTA0OTIxMDMzMDY4NTU0.YmojSg.rM_PgmWlNG_DPepaTHWGcN9t7nk";
-	
+
+	private final static String token = "OTY5MTA0OTIxMDMzMDY4NTU0.YmojSg.DcT3J8Icln1hfJXY0ZHuX35foTM";
+
 	public static void main(String[] args) throws LoginException {
-		  JDA jda = JDABuilder.createDefault(token).build();
-	       
-	        jda.addEventListener(new DiscordBot());
+		JDA jda = JDABuilder.createDefault(token).build();
+
+		jda.addEventListener(new DiscordBot());
 	}
-	 	@Override
-	    public void onMessageReceived(MessageReceivedEvent event){
-	 		 
-	 	    LolApiUtils api = new LolApiUtils();
-	 		 
-	 	    Message msg = event.getMessage();
-	 	    MessageChannel channel = event.getChannel();
-	 		 
-	 	    if(event.getAuthor().isBot()) {
-	 	    	return;
-	 	    }
-	 		 if(msg.getContentRaw().contains("!전적검색")) {
-	 			isCalled = true;
-	 			 channel.sendMessage("닉네임을 입력해주세요.🙂").queue();
-	 		 }
-	 		 if(isCalled == true && !msg.getContentRaw().contains("!")) {
-	 			 JSONObject result = api.getSummonerInfo(msg.getContentRaw());
-	 			 channel.sendMessage("소환사이름"+":"+result.get("summonerName")+","+"레벨"+":"+result.get("summonerLevel")
-	 			 +"랭크" +":"+ result.get("tier")+","+result.get("rank")+","+"LP"+result.get("leaguePoints") +","+"승" +":" +result.get("wins") + "패" +":"+ result.get("losses")).queue();
-	 			 
-	 			 isCalled = false;
-	 		 }
-	         
-	    }
+
+	@Override
+	public void onMessageReceived(MessageReceivedEvent event) {
+
+		LolApiUtils api = new LolApiUtils();
+
+		Message msg = event.getMessage();
+		MessageChannel channel = event.getChannel();
+
+		if (event.getAuthor().isBot()) {
+			return;
+		}
+		if (msg.getContentRaw().contains("!전적검색")) {
+			isCalled = true;
+			channel.sendMessage("닉네임을 입력해주세요.🙂").queue();
+		}
+		if (isCalled == true && !msg.getContentRaw().contains("!")) {
+			JSONObject result = api.getSummonerInfo(msg.getContentRaw());
+			channel.sendMessage(
+					"소환사이름" + ":" + result.get("summonerName") + "," + "레벨" + ":" + result.get("summonerLevel")
+							+ "랭크" + ":" + result.get("tier") + "," + result.get("rank") + "," + "LP"
+							+ result.get("leaguePoints") + "," + "승" + ":" + result.get("wins") + "패" + ":"
+							+ result.get("losses"))
+					.queue();
+
+			isCalled = false;
+		}
+
+	}
 
 }
